801a7b61f55217564e661e34165ddc6f7e6d40fe
Filipe Fernandes <filipebsb04@gmail.com>
2016-12-05 18:33:32 -0200

    capitulo05-aula03 [v2]

--- app/app.ts
+++ app/app.ts
@@ -7,6 +7,7 @@ import {MenuTestPage} from './pages/menu-test/menu-test';
 import {GeneratedTestPage} from './pages/generated-test/generated-test';
 import {AlertPage} from './pages/alert/alert';
 import {ButtonTestPage} from './pages/button-test/button-test';
+import {CardTestPage} from './pages/card-test/card-test';
 
 
 @Component({
@@ -24,7 +25,8 @@ export class MyApp {
       {component:MenuTestPage, title: 'Menu Test', icon: 'cart'},
       {component:GeneratedTestPage, title: 'Generated Page', icon: 'arrow-forward'},
       {component:AlertPage, title: 'Alert', icon: 'md-bulb'},
-      {component:ButtonTestPage, title: 'button Test', icon: 'md-pricetag'}
+      {component:ButtonTestPage, title: 'button Test', icon: 'md-pricetag'},
+      {component:CardTestPage, title: 'card test', icon: 'md-card'}
     ];
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
